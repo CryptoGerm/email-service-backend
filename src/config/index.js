46b60b0bd64eb79ba645e16864f565d7ec3ae513
Stuti Chaturvedi <62954748+CryptoGerm@users.noreply.github.com>
2021-06-27 13:57:15 +0530

    fixed mongoURI

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ import * as pkg from '../../package.json';
 dotEnv.config({ path: join(__dirname, '../../.env') });
 
 const config = {
-  nodeEnv: process.env.NODE_ENV || 'development',
+  nodeEnv: process.env.NODE_ENV,
   jwt: {
     secret: process.env.JWT_SECRET,
     accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION,
@@ -23,9 +23,10 @@ const config = {
   sendGridKey: process.env.SENDGRID_API_KEY,
   db: {
     uri:
-      process.env.NODE_ENV === 'development'
-        ? `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}`
-        : `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}`,
+      // process.env.NODE_ENV === 'development'
+      //   ? `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}`
+      //   :
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`,
     dbName: process.env.MONGO_DBNAME,
   },
   googleOAuth: {
